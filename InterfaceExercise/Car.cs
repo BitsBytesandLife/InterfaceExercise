c3b68e64cd787aa4e570540c65f1a2fbc1cf6e36
BK <brian_j_king@me.com>
2020-09-09 10:31:16 -0500

    Add the Car class

---
+++ InterfaceExercise/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExercise
+{
+    public class Car : IVehicle, ICompany
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public int Wheels { get; set; }
+        public string Name { get; set; }
+        public string Logo { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine($"The {Model} is in drive.");
+        }
+    }
+}
